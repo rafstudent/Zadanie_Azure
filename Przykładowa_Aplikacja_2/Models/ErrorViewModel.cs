1a90f6fe12c93114f7838cb14ac956e7943e7418
rafstudent <37633394+rafstudent@users.noreply.github.com>
2021-03-08 19:28:03 +0100

    Zadanie

---
+++ Przykładowa_Aplikacja_2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Przykładowa_Aplikacja_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
